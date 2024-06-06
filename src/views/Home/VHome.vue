e4ef54c77657c05839318ea1a63a323224aa2606
nhat.nguyen <nhat0x9@gmail.com>
2024-06-06 21:22:30 +0700

    feat: create navbar

--- src/views/Home/VHome.vue
+++ src/views/Home/VHome.vue
@@ -1,54 +1,40 @@
 <template>
-  <div class="">
-    <Menubar :model="items">
-      <!-- <template #start>
-        <svg
-          width="35"
-          height="40"
-          viewBox="0 0 35 40"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-2rem"
-        >
-          <path d="..." fill="var(--primary-color)" />
-          <path d="..." fill="var(--text-color)" />
-        </svg>
-      </template> -->
-      <template #item="{ item, props, hasSubmenu, root }">
-        <a v-ripple class="flex align-items-center" v-bind="props.action">
-          <span :class="item.icon" />
-          <span class="ml-2">{{ item.label }}</span>
-          <Badge
-            v-if="item.badge"
-            :class="{ 'ml-auto': !root, 'ml-2': root }"
-            :value="item.badge"
-          />
-          <span
-            v-if="item.shortcut"
-            class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1"
-            >{{ item.shortcut }}</span
-          >
-          <i
-            v-if="hasSubmenu"
-            :class="[
-              'pi pi-angle-down',
-              { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }
-            ]"
-          ></i>
-        </a>
-      </template>
-      <template #end>
-        <div class="flex align-items-center gap-2">
-          <InputText placeholder="Search" type="text" class="w-8rem sm:w-auto" />
-          <Avatar image="/images/avatar/amyelsner.png" shape="circle" />
+  <div class="app">
+    <nav>
+      <div class="navbar">
+        <div class="container nav-container">
+          <input class="checkbox" type="checkbox" name="" id="" />
+          <div class="hamburger-lines">
+            <span class="line line1"></span>
+            <span class="line line2"></span>
+            <span class="line line3"></span>
+          </div>
+          <div class="logo">
+            <h1>Navbar</h1>
+          </div>
         </div>
-        <Button label="Submit" />
+      </div>
+    </nav>
+    <div class="body">
+      <div class="menu">
+        <div class="menu-items">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">about</a></li>
+          <li><a href="#">blogs</a></li>
+          <li><a href="#">portfolio</a></li>
+          <li><a href="#">contact</a></li>
+        </div>
+      </div>
+      <div class="main">
+        content
+      </div>
+    </div>
 
-      </template>
-    </Menubar>
-  </div>
-  <div class="home-body">
-    <RouterView />
+
+
+
+
+
   </div>
 </template>
 
@@ -74,65 +60,7 @@ export default {
     Badge
   },
 
-  setup(props) {
-    const items = ref([
-      {
-        label: 'Home',
-        icon: 'pi pi-home'
-      },
-      {
-        label: 'Features',
-        icon: 'pi pi-star'
-      },
-      {
-        label: 'Projects',
-        icon: 'pi pi-search',
-        items: [
-          {
-            label: 'Core',
-            icon: 'pi pi-bolt',
-            shortcut: '⌘+S'
-          },
-          {
-            label: 'Blocks',
-            icon: 'pi pi-server',
-            shortcut: '⌘+B'
-          },
-          {
-            label: 'UI Kit',
-            icon: 'pi pi-pencil',
-            shortcut: '⌘+U'
-          },
-          {
-            separator: true
-          },
-          {
-            label: 'Templates',
-            icon: 'pi pi-palette',
-            items: [
-              {
-                label: 'Apollo',
-                icon: 'pi pi-palette',
-                badge: 2
-              },
-              {
-                label: 'Ultima',
-                icon: 'pi pi-palette',
-                badge: 3
-              }
-            ]
-          }
-        ]
-      },
-      {
-        label: 'Contact',
-        icon: 'pi pi-envelope',
-        badge: 3
-      }
-    ])
-
-    return { items }
-  },
+  setup(props) { },
 
   methods: {
     logout() {
@@ -147,3 +75,182 @@ export default {
   computed: {}
 }
 </script>
+
+<style lang="scss">
+body {
+  font-family: "Poppins", sans-serif;
+}
+
+.container {
+  max-width: 1360px;
+  // width: 90%;
+  margin: auto;
+}
+
+.navbar {
+  width: 100%;
+  box-shadow: 0 1px 4px rgb(146 161 176 / 15%);
+}
+
+.nav-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 62px;
+}
+
+.navbar .menu-items {
+  display: flex;
+}
+
+.navbar .nav-container li {
+  list-style: none;
+}
+
+.navbar .nav-container a {
+  text-decoration: none;
+  color: #0e2431;
+  font-weight: 500;
+  font-size: 1.2rem;
+  padding: 0.7rem;
+}
+
+.navbar .nav-container a:hover {
+  font-weight: bolder;
+}
+
+.nav-container {
+  display: block;
+  position: relative;
+  height: 60px;
+}
+
+.nav-container .checkbox {
+  position: absolute;
+  display: block;
+  height: 32px;
+  width: 32px;
+  top: 20px;
+  left: 20px;
+  z-index: 5;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.nav-container .hamburger-lines {
+  display: block;
+  height: 26px;
+  width: 32px;
+  position: absolute;
+  top: 17px;
+  left: 20px;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.nav-container .hamburger-lines .line {
+  display: block;
+  height: 4px;
+  width: 100%;
+  border-radius: 10px;
+  background: #0e2431;
+}
+
+.nav-container .hamburger-lines .line1 {
+  transform-origin: 0% 0%;
+  transition: transform 0.4s ease-in-out;
+}
+
+.nav-container .hamburger-lines .line2 {
+  transition: transform 0.2s ease-in-out;
+}
+
+.nav-container .hamburger-lines .line3 {
+  transform-origin: 0% 100%;
+  transition: transform 0.4s ease-in-out;
+}
+
+.logo {
+  position: absolute;
+  top: 5px;
+  right: 15px;
+  font-size: 1.2rem;
+  color: #0e2431;
+}
+
+.nav-container input[type="checkbox"]:checked~.menu-items {
+  transform: translateX(0);
+}
+
+.nav-container input[type="checkbox"]:checked~.hamburger-lines .line1 {
+  transform: rotate(45deg);
+}
+
+.nav-container input[type="checkbox"]:checked~.hamburger-lines .line2 {
+  transform: scaleY(0);
+}
+
+.nav-container input[type="checkbox"]:checked~.hamburger-lines .line3 {
+  transform: rotate(-45deg);
+}
+
+// menu
+.main {
+  // width: 1080px;
+  width: 100%;
+  border: 1px solid black;
+  z-index: 1;
+  background-color: white;
+}
+
+.menu {
+  transform: translateX(100%);
+  transition: transform 0.3s ease-in-out;
+  width: 0px;
+  z-index: -1;
+}
+.menu-items {
+  width: 300px;
+}
+
+.body {
+  display: flex;
+  margin: 0 auto;
+  max-width: 1360px;
+  transition: max-width 0.3s ease-in-out;
+  border: 1px solid black;
+  height: 100vh;
+}
+
+.app:has(input[type="checkbox"]:checked) {
+  .body {
+    .menu {
+      transform: translateX(0);
+      transition: transform 0.3s ease-in-out;
+      transition: width 0.3s ease-in-out;
+      width: 300px;
+    }
+  }
+
+  .navbar .container {
+    transition: max-width .3s ease-in-out;
+  }
+}
+
+.app:has(input[type="checkbox"]:not(:checked)) {
+  .menu {
+    transition: all .3s ease-in-out;
+  }
+
+  .body {
+    max-width: 1080px;
+  }
+
+  .navbar .container {
+    max-width: 1080px;
+    transition: max-width .3s ease-in-out;
+  }
+}
+</style>
