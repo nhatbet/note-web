542e6623df446bbabd309509dc5b3d7f1fcc421f
木村 邦彦 <kimura_dev@futureantiques.co.jp>
2024-06-25 17:22:51 +0700

    feat: component article show

---
+++ src/views/Article/VArticleShow.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script lang='ts'>
+export default {
+    name: 'VArticleShow',
+    props: {},
+    components: {},
+    setup(props, { emit }) {
+        return {}
+    },
+
+    methods: {}
+}
+</script>
