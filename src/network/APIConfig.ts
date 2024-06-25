040365568c84d4242cb87b386042afdc0f012870
木村 邦彦 <kimura_dev@futureantiques.co.jp>
2024-06-25 14:55:13 +0700

    feat: handle error 401 for api

--- src/network/APIConfig.ts
+++ src/network/APIConfig.ts
@@ -1,7 +1,8 @@
-import axios from 'axios';
-import LocalStorageService from '@/services/LocalStorageService';
+import axios from 'axios'
+import LocalStorageService from '@/services/LocalStorageService'
+import { useAuthStore } from '@/stores/auth'
 
-const baseURL = import.meta.env.VITE_HOST + '/api';
+const baseURL = import.meta.env.VITE_HOST + '/api'
 
 const api = axios.create({
     baseURL: baseURL,
@@ -9,25 +10,41 @@ const api = axios.create({
     headers: {
         'Content-Type': 'multipart/form-data',
         'Access-Control-Allow-Origin': '*',
-        'Authorization': LocalStorageService.getBearerToken()
+        Authorization: LocalStorageService.getBearerToken()
     }
 })
 // headers['Content-Type'] = 'application/json';
-api.interceptors.response.use((response) => {
-    const status = response.data.status;
-    const data = response.data;
-    if (status == '200') {
-        return data;
+api.interceptors.response.use(
+    (response) => {
+        const status = response.data.status
+        const data = response.data
+        if (status == '200') {
+            return data
+        }
+
+        return Promise.reject(data)
+    },
+    (error) => {
+        console.log('error', error)
+
+        if (error && error.response && [401, 403].includes(error.response.status)) {
+            // handle error
+            if (error.response.status) {
+                switch (error.response.status) {
+                    case 401:
+                        const authStore = useAuthStore()
+                        authStore.setIsAuthenticated(true)
+                        break
+                    case 403:
+                        // handle permission
+                        break
+                    default:
+                    // code block
+                }
+            }
+        }
+        return Promise.reject(error)
     }
+)
 
-    return Promise.reject(data);
-}, (error) => {
-    console.log('error', error);
-
-    if (error && error.response && [401, 403].includes(error.response.status)) {
-        // handle error
-    }
-    return Promise.reject(error);
-});
-
-export default api;
+export default api
