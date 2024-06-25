d28477ac9915fe24a265294de6804cc9c612deb8
木村 邦彦 <kimura_dev@futureantiques.co.jp>
2024-06-25 08:36:44 +0700

    feat: add config language for mdeditor

--- src/components/Editor/MDEditor.vue
+++ src/components/Editor/MDEditor.vue
@@ -19,9 +19,96 @@
 
 <script lang='ts'>
 import { computed, ref } from 'vue'
-import { MdEditor } from 'md-editor-v3'
+import { MdEditor, config } from 'md-editor-v3'
 import 'md-editor-v3/lib/style.css'
-
+config({
+    editorConfig: {
+        languageUserDefined: {
+            'en-US': {
+                toolbarTips: {
+                    bold: 'bold',
+                    underline: 'underline',
+                    italic: 'italic',
+                    strikeThrough: 'strikeThrough',
+                    title: 'title',
+                    sub: 'subscript',
+                    sup: 'superscript',
+                    quote: 'quote',
+                    unorderedList: 'unordered list',
+                    orderedList: 'ordered list',
+                    task: 'task list',
+                    codeRow: 'inline code',
+                    code: 'block-level code',
+                    link: 'link',
+                    image: 'image',
+                    table: 'table',
+                    mermaid: 'mermaid',
+                    katex: 'formula',
+                    revoke: 'back',
+                    next: 'forward',
+                    save: 'save',
+                    prettier: 'prettier',
+                    pageFullscreen: 'fullscreen in page',
+                    fullscreen: 'fullscreen',
+                    preview: 'preview',
+                    previewOnly: 'previewOnly',
+                    htmlPreview: 'html preview',
+                    catalog: 'catalog',
+                    github: 'source code'
+                },
+                titleItem: {
+                    h1: 'Lv1 Heading',
+                    h2: 'Lv2 Heading',
+                    h3: 'Lv3 Heading',
+                    h4: 'Lv4 Heading',
+                    h5: 'Lv5 Heading',
+                    h6: 'Lv6 Heading'
+                },
+                imgTitleItem: {
+                    link: 'Add Img Link',
+                    upload: 'Upload Img',
+                    clip2upload: 'Clip Upload'
+                },
+                linkModalTips: {
+                    linkTitle: 'Add Link',
+                    imageTitle: 'Add Image',
+                    descLabel: 'Desc:',
+                    descLabelPlaceHolder: 'Enter a description...',
+                    urlLabel: 'Link:',
+                    urlLabelPlaceHolder: 'Enter a link...',
+                    buttonOK: 'OK'
+                },
+                clipModalTips: {
+                    title: 'Crop Image',
+                    buttonUpload: 'Upload'
+                },
+                copyCode: {
+                    text: 'Copy',
+                    successTips: 'Copied!',
+                    failTips: 'Copy failed!'
+                },
+                mermaid: {
+                    flow: 'flow',
+                    sequence: 'sequence',
+                    gantt: 'gantt',
+                    class: 'class',
+                    state: 'state',
+                    pie: 'pie',
+                    relationship: 'relationship',
+                    journey: 'journey'
+                },
+                katex: {
+                    inline: 'inline',
+                    block: 'block'
+                },
+                footer: {
+                    markdownTotal: 'Word Count',
+                    scrollAuto: 'Scroll Auto'
+                }
+            }
+        }
+    }
+})
 export default {
     name: 'MDEditor',
     components: {
@@ -65,6 +152,7 @@ export default {
                 emit('update:modelValue', value)
             }
         })
+
         const toolbars = ref([
             'revoke',
             'next',
@@ -98,9 +186,8 @@ export default {
             'htmlPreview',
             'catalog'
         ])
-        const emojis = ['😀', '😃']
 
-        return { model, toolbars, emojis }
+        return { model, toolbars }
     },
 
     methods: {
