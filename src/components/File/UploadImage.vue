569cc921e8693147d82f18dea7c8db7294ff0265
木村 邦彦 <kimura_dev@futureantiques.co.jp>
2024-07-16 03:45:58 +0700

    feat: install vue-advanced-cropper

---
+++ src/components/File/UploadImage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <cropper
+            class="cropper"
+            src="https://images.pexels.com/photos/226746/pexels-photo-226746.jpeg"
+            :stencil-props="{
+                aspectRatio: 1 / 1
+            }"
+            @change="change"
+        ></cropper>
+    </div>
+</template>
+
+<script lang="ts">
+import { computed, ref } from 'vue'
+import { Cropper } from 'vue-advanced-cropper'
+import 'vue-advanced-cropper/dist/style.css'
+
+export default {
+    name: 'UploadImage',
+    components: {
+        Cropper
+    },
+    props: {},
+    setup(props, { emit }) {
+        const img = ref('https://images.pexels.com/photos/226746/pexels-photo-226746.jpeg')
+
+        return { img }
+    },
+    methods: {
+        change({ coordinates, canvas }) {
+            console.log(coordinates, canvas)
+            console.log('changed');
+            
+        }
+    }
+}
+</script>
+<style lang="scss"></style>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -2,11 +2,13 @@
     <div class="w-full">
         <!-- <CSelectSearch v-model="tags" :multipleSelect="true" label="Tags" :options="selection.article_status"
             placeholder="Select tag"></CSelectSearch> -->
+        <UploadImage></UploadImage>
     </div>
 
 </template>
 <script setup lang='ts'>
 import { ref } from 'vue'
+import UploadImage from '@/components/File/UploadImage.vue'
 
 const tags = ref([]);
 
