ab8046329768a34e91ae840ea80e266ec7e8e4cb
木村 邦彦 <kimura_dev@futureantiques.co.jp>
2024-07-22 09:15:26 +0700

    feedback: change home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+const gotoHome = () => {
+  router.push({ name: 'VHome'})
+}
+
 </script>
 
 <template>
+  <CButton text="Home" classes="!w-[100px] my-5" @clickCButton="gotoHome()"></CButton>
   <main>
     <TheWelcome />
   </main>
