2845254343fabf13cfce6234363ef411cb0c485f
木村 邦彦 <kimura_dev@futureantiques.co.jp>
2024-07-29 13:22:02 +0700

    feat: avatar common

---
+++ src/components/General/CAvatar.vue
@@ -0,0 +1,23 @@
+<template>
+    <img
+        v-if="src"
+        :src="src"
+        class="w-[24px] h-[24px] rounded-full mx-1 inline-block"
+        :class="classes"
+        alt="avatar"
+    />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    src: {
+        type: String,
+        default: ''
+    },
+    classes: {
+        type: [String, Array],
+        default: ''
+    }
+})
+</script>
+<style scoped lang="scss"></style>
