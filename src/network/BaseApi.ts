6c23885e0045e53724e416015e19f616a36f1650
nhathdtk <nhat0x9@gmail.com>
2024-10-31 22:57:48 +0700

    feed: add param to header

--- src/network/BaseApi.ts
+++ src/network/BaseApi.ts
@@ -10,7 +10,9 @@ class BaseApi {
     public headers: any = {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
-        'Cache-Control': 'no-cache, must-revalidate',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
         'Authorization': 'No auth'
     }
     public params: object | null = null;
